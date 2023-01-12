20714bf256f89bd33224b7891d1b4ac5d568e029
Otto Vänttinen <kingultimateman@gmail.com>
2023-01-12 13:42:58 +0200

    Add project files.

---
+++ wazotool_proto/Core/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wazotool_proto.Core
+{
+    class RelayCommand : ICommand
+    {
+
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }   
+    }
+}
