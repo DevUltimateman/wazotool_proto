a237922dd309b256ca9c0894dacd84af9c32a89c
Otto Vänttinen <kingultimateman@gmail.com>
2023-01-12 23:08:44 +0200

    up

---
+++ wazotool_proto/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wazotool_proto.Core;
+
+namespace wazotool_proto.MVVM.ViewModel
+{
+    class MainViewModel :ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
